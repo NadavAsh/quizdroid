5a04b8c16f11690e16d4206d668832189becccec
Nadav Ashkenazi <ashkenazi.nadav@gmail.com>
2015-05-07 18:27:48 -0700

    Added app object, quiz model objects, and topic repository interface.

---
+++ app/src/main/java/edu/washington/nadava/quizdroid/Question.java
@@ -0,0 +1,48 @@
+package edu.washington.nadava.quizdroid;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by nadavash on 5/7/15.
+ */
+public class Question {
+    private String question;
+    private ArrayList<String> answers;
+    private int correct;
+
+    public Question() {
+
+    }
+
+    public Question(String question, ArrayList<String> answers, int correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String value) {
+        question = value;
+    }
+
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(ArrayList<String> value) {
+        answers = value;
+    }
+
+    public int getCorrect() {
+        return correct;
+    }
+
+    public void getCorrect(int value) {
+        correct = value;
+    }
+}
---
+++ app/src/main/java/edu/washington/nadava/quizdroid/QuizApp.java
@@ -0,0 +1,7 @@
+package edu.washington.nadava.quizdroid;
+
+/**
+ * Created by nadavash on 5/7/15.
+ */
+public class QuizApp {
+}
